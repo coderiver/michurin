db2e96d33e9765fa5c739897c6deb49a65f95b32
dpismenny <denis.pismenny@gmail.com>
2013-03-25 14:33:48 +0200

    main page

--- js/common.js
+++ js/common.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
 
+	// masonry
+	  $(document).ready(function(){
+	    $('#container').masonry({
+	// указываем элемент-контейнер в котором расположены блоки для динамической верстки
+	      itemSelector: '.item',
+	// указываем класс элемента являющегося блоком в нашей сетке
+	          singleMode: false,
+	// true - если у вас все блоки одинаковой ширины
+	      isResizable: true,
+	// перестраивает блоки при изменении размеров окна
+	      isAnimated: true,
+	// анимируем перестроение блоков
+	          animationOptions: {
+	          queue: false,
+	          duration: 500
+	      }
+	// опции анимации - очередь и продолжительность анимации
+	    });
+	  });
+
+	var $container = $('#container');
+
+	$container.imagesLoaded( function(){
+	  $container.masonry({
+	    itemSelector : '.item'
+	  });
+	});
+
 });
